a89d97fc5f351bec1530c1b993fd911115ac13da
pranavrao145 <56097527+pranavrao145@users.noreply.github.com>
2021-04-08 13:48:50 -0400

    changed error handling in define.js

--- commands/define.js
+++ commands/define.js
@@ -1,16 +1,22 @@
 const dictionary = require("word-definition");
 
 module.exports = {
-  name: "define",
-  alias: ["df"],
-  description: "Define a word.",
-  execute(msg, args) {
-      dictionary.getDef(args[0].toLowerCase(), "en", null, (definition) => {
-        if (definition["definition"] != undefined) {  
-            msg.reply("definition of *" + definition["word"] + "* (" + definition["category"] + "): " + definition["definition"]);
+    name: "define",
+    alias: ["df"],
+    description: "Define a word.",
+    execute(msg, args) {
+        if (args[0]) {
+            dictionary.getDef(args[0].toLowerCase(), "en", null, (definition) => {
+                if (definition["definition"] != undefined) {
+                    msg.reply("definition of *" + definition["word"] + "* (" + definition["category"] + "): " + definition["definition"]);
+                } else {
+                    msg.reply("no definition found for *" + definition["word"] + "*. " + "Please check your spelling and try again.")
+                }
+            })
         }
         else {
-            msg.reply("no definition found for *" + definition["word"] + "*. " + "Please check your spelling and try again.")
+            msg.reply("incorrect syntax! Correct syntax: f!define {word}");
         }
-      })
-}}
+
+    }
+}
