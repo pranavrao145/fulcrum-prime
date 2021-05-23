64b042545722717e210dce4156d8926d3b750981
Pranav Rao <pranavrao145@gmail.com>
2021-05-23 15:00:23 -0400

    updated docker config

---
+++ init.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS channelcountchannel(
+    guildid VARCHAR(18) NOT NULL,
+    channelid VARCHAR(18) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS membercountchannel(
+    guildid VARCHAR(18) NOT NULL,
+    channelid VARCHAR(18) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS datechannel(
+    guildid VARCHAR(18) NOT NULL,
+    channelid VARCHAR(18) NOT NULL
+);
+
+
