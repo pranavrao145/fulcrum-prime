c889818a785df91f43cbeb873b261a68addca55e
Pranav Rao <pranavrao145@gmail.com>
2021-05-25 16:01:52 -0400

    formatting changes

--- commands/create_channel.js
+++ commands/create_channel.js
@@ -1,7 +1,8 @@
 module.exports = {
     name: "create_channel",
     description: "Allows admins to create a channel.",
-    execute(message, args) { if (!message.member.hasPermission("MANAGE_CHANNELS")) {
+    execute(message, args) {
+        if (!message.member.hasPermission("MANAGE_CHANNELS")) {
             message.reply("sorry, you need the MANAGE_CHANNELS permission to use this command.").catch();
             return;
         }
