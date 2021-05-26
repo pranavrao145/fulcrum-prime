5ea95bb849b35f0a6db468bbf935f747ab26d2f6
Pranav Rao <pranavrao145@gmail.com>
2021-05-25 20:53:49 -0400

    updated help message

--- commands/help.js
+++ commands/help.js
@@ -143,6 +143,11 @@ module.exports = {
                         value:
                             "Alias: cc\nCreates a channel with a name, and optional type and permissions specified. By default, the value for type will be 'text', but you can change it by specifically passing 'voice'. Similarly, the permission for the channel will be public by default, but you can explicitly pass private to make the channel invisible to the @everyone role.",
                     },
+                    {
+                        name: "deletechannel [channel mention (vc role for voice channels)]",
+                        value:
+                            "Alias: dc\nDeletes the channel specified.",
+                    },
                 )
                 .setTimestamp()
             channel.send(embed).catch()
